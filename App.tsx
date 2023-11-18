4c27b99ccbc26faef930641619f82915541f370d
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2023-11-17 23:56:06 -0300

    feat: creating add new annotation screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
+import 'react-native-get-random-values';
 import React from 'react';
 
 import {ThemeProvider} from 'styled-components/native';
 
 import theme from './src/theme';
 
-import Home from './src/screens/Home';
+import AddAnnotation from './src/screens/AddAnnotation';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <AddAnnotation />
     </ThemeProvider>
   );
 }
